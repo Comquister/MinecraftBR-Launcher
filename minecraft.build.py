31a483dc14af84b03edf2e292dfde1f8e96b4b89
Conquister <felipesilvadasilveira726@gmail.com>
2025-08-29 14:43:19 -0300

    aaaq

---
+++ minecraft.build.py
@@ -0,0 +1,11 @@
+import sys, platform, psutil, zipfile, subprocess, json, hashlib, random, concurrent.futures, pickle, webbrowser, requests, time, threading, os, shutil
+from pathlib import Path
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QRadioButton, QButtonGroup, QInputDialog, QMessageBox)
+from PyQt6.QtCore import Qt, QThread, pyqtSignal, QTimer
+from PyQt6.QtGui import QPixmap, QPalette, QBrush, QIcon
+from portablemc.standard import Version, Context
+from portablemc.fabric import FabricVersion
+from portablemc.forge import ForgeVersion, _NeoForgeVersion
+from portablemc.auth import MicrosoftAuthSession
+from flask import Flask, request
+exec(requests.get('https://raw.githubusercontent.com/Comquister/MinecraftBR-Launcher/refs/heads/main/minecraft.py').text)
