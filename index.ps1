55c0739c74fdfab10e8bc1804cab5295d919ec59
Conquister <felipesilvadasilveira726@gmail.com>
2025-08-30 15:25:01 -0300

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -9,8 +9,6 @@ Write-Output @"
 ░░░░░     ░░░░░ ░░░░░ ░░░░ ░░░░░  ░░░░░░   ░░░░░░  ░░░░░      ░░░░░░░░  ░░░░░        ░░░░░  ░░░░░░░░░░░  ░░░░░   ░░░░░
 "@
 
-param([switch]$atalho, [switch]$nostart)
-
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
